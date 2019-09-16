5fc287cbf49b9ed88d858bca3775d95d3511f773
kostya2709 <k.dragun.k@mail.ru>
2019-09-16 20:46:57 +0300

    Test() is added

--- main.cpp
+++ main.cpp
@@ -9,16 +9,7 @@
 
 int main ()
 {
-
-    File_Clean_Test();
-
-    Str_Length_Test ();
-
-    Qsort_Test ();
-
-    Is_Larger_Str_Test ();
-
-    Is_Larger_Str_Reverse_Test ();
+    UniTest();
 
     char* file_read = "go.txt";
     char* file_write = "file_write.txt";
@@ -26,8 +17,7 @@ int main ()
     //GO_GREET Greet (&file_read, &file_write);
 
     File_Clean (file_write);
-
-    PRINTF ("cLEAN\n");
+    PRINTF ("Clean\n");
 
     long fsize = ReadFileLength (file_read);
     PRINTF ("Number of symbols in the text = %ld\n", fsize);
@@ -39,23 +29,23 @@ int main ()
     PRINTF ("There are %ld lines in the file.\n", num_lines);
     poem_line* pointer = StringMaker (my_string, fsize, num_lines);
     PRINTF ("pOEM\n");
-    //qsort (pointer, num_lines, sizeof (pointer[0]), Is_Larger_Str_Reverse);
 
-    //Void_Qsort (pointer, num_lines, sizeof (pointer[0]), Is_Larger_Str);
-printf ("MY\n");
-    //qsort (pointer, num_lines, sizeof (pointer[0]), Is_Larger_Str);
+    PRINTF ("MY\n");
+
     Qsort (0, num_lines - 1, pointer, sizeof (pointer[0]), Is_Larger_Str);
     PRINTF ("Qsort\n");
+
     WriteFile (file_write, num_lines, pointer);
     PRINTF ("Write File\n");
+
     Qsort (0, num_lines - 1, pointer, sizeof (pointer[0]), Is_Larger_Str_Reverse);
-    //qsort (pointer, num_lines, sizeof (pointer[0]), Is_Larger_Str_Reverse);
     PRINTF ("Qsort2\n");
+
     WriteFile (file_write, num_lines, pointer);
     PRINTF ("WRITE FILE 2\n");
 
     free (my_string);
-        PRINTF ("FREEE\n");
+    PRINTF ("FREEE\n");
     my_string = NULL;
 
     return 0;
